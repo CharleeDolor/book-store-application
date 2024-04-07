33d95a403124a2c31b337b8c8e1076d43f3df2a7
Crissan Dondriano <dondrianocrissan20@gmail.com>
2024-04-07 23:01:51 +0800

    Simple Function Addons - Dondriano

---
+++ src/components/UserAuthentication.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+      <h1>User Login</h1>
+      <div v-if="!isAuthenticated">
+        <form @submit.prevent="login">
+          <input type="text" v-model="credentials.username" placeholder="Username" />
+          <input type="password" v-model="credentials.password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+        <p v-if="error">{{ error }}</p>
+      </div>
+      <div v-else>
+        <p>Welcome back, user!</p>
+        <button @click="logout">Logout</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        credentials: {
+          username: '',
+          password: ''
+        },
+        error: null,
+        isAuthenticated: false
+      };
+    },
+    methods: {
+      login() {
+        this.error = null; 
+        this.$store.dispatch('simulateAuthentication', this.credentials)
+          .then(() => {
+            this.isAuthenticated = true;
+          })
+          .catch(error => {
+            this.error = error; // Display error message
+          });
+      },
+      logout() {
+        this.$store.commit('updateAuthenticationStatus', false);
+        this.isAuthenticated = false;
+      }
+    },
+    created() {
+      this.isAuthenticated = this.$store.state.isAuthenticated;
+    }
+  };
+  </script>
+
+  <style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <authentication-user/>
+    <book-list/>
+    <shopping-cart/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import UserAuthentication from '@/components/UserAuthentication.vue'
+import BookList from '@/components/BookList.vue'
+import ShoppingCart from '@/components/ShoppingCart.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    'authentication-user': UserAuthentication,
+    'book-list': BookList,
+    'shopping-cart': ShoppingCart
   }
 }
 </script>
