820bd25a07d110c6ded05e3bbad6797785f4ab1d
Charlee_Dolor <dolorcharlee55@gmail.com>
2024-04-10 21:37:52 +0800

    minor merging conflict fix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 import './assets/main.css'
->>>>>>> f7b8410af6bb3e0548200566d2a3cc721ce89d19
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
